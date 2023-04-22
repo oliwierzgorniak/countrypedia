f51b24e220bccd9e3afb546483e9de7085c5fea0
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2023-04-22 11:51:52 +0200

    SERVER_URL changed to production

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,3 +1,3 @@
 export const NUMBER_OF_LIST_ELEMENTS = 6;
-export const SERVER_URL = "http://localhost:3000?country=";
+export const SERVER_URL = "https://countrypedia.onrender.com:3000?country=";
 export const SELECTED_LIST_ELEMENT_COLOR = "#e5e5e5";

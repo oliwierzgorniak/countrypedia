4c84f40c510a6621f7ade39a5c3e9906547fd227
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2023-04-22 14:12:51 +0200

    api variables moved from .env to ts file

---
+++ backend/src/consts.ts
@@ -0,0 +1,2 @@
+export const API_URL = "https://restcountries.com/v3.1/name/";
+export const API_PARAM = "?fullText=true";
--- backend/src/lib/getApiUrl.ts
+++ backend/src/lib/getApiUrl.ts
@@ -1,10 +1,5 @@
+import { API_URL, API_PARAM } from "../consts";
+
 export default function getApiUrl(name: string) {
-  const url = process.env.API_URL;
-  const param = process.env.API_PARAM;
-
-  if (typeof url !== "string" || typeof param !== "string") {
-    throw new Error(".env variables not available");
-  }
-
-  return `${process.env.API_URL}${name}${process.env.API_PARAM}`;
+  return `${API_URL}${name}${API_PARAM}`;
 }
